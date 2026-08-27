1682d372ba8948423700adbbf29b4a418df873b4
Cobraa315 <34480100@nebraska.edu>
2026-08-27 10:40:57 -0500

    added my name and major

--- hello.c
+++ hello.c
@@ -7,7 +7,9 @@
 int main (int argc, char **argv) {
 
     printf ("Hello, I'm Samyak Jain!\n");
+    printf ("Hi!! My name is Chase Tarwater!\n");
     printf ("Major is Computer Science!\n");
+    printf ("Chase's Major is Computer Engineering\n");
     printf ("Minor in Finance\n");
 
     return 0;
